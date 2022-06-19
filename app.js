929d00b48ca7161ae631a4c64c2974119c46a6b8
Aleksander <sanyok.10rus@gmail.com>
2022-06-20 00:46:30 +0300

    it was made  a bar for app

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+//https://www.youtube.com/watch?v=lzQIhjElV_g
+//29:00
+////////////////////////////////////////////////////////////
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('*', (req, res) => {
